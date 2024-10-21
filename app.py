2ec5018a5ad76fd1549879b609f1beca46f94dd6
srmiranha <132907964+SrMiranha@users.noreply.github.com>
2024-10-21 10:11:41 -0300

    flask 02

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from  flask import render_template
+from flask import request
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1> olá <br> mundo! </h1>"
 
-@app.route("/aluno/<nome>")
-def aluno(nome):
-    return f'<h1>{nome}</h1>'
+@app.route("/aluno")
+def aluno():
+    return render_template("formulario.html")
+
+@app.route("/enviar", methods=["POST"])
+def envioform():
+    nombre = request.form['nome']
+    senha = request.form['senha']
+
+    if senha == '123':
+       return render_template('aluno.html', n=nombre)
+    else:
+        return "se fudeu"
